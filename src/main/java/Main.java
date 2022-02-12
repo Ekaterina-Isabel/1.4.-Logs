4de94f2afe88140fec7fc73b939568a7da490d24
kate <vuk_kat@mail.ru>
2022-02-12 10:57:20 +0500

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+//    service.calculate(amount, registered);
+      long result = service.calculate(amount, registered);
+      System.out.println(result);
   }
 }
-
