78b65b1f1bae287f9439f8a34cef68ff795a2ea1
kate <vuk_kat@mail.ru>
2022-02-12 09:55:41 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 1000_60;
+    boolean registered = true;
+
+    service.calculate(amount, registered);
+  }
+}
+
